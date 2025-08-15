dac7535961524b29199e40c7b949b0817704e534
XitaRps <xitarps@gmail.com>
2025-08-15 19:04:37 -0300

    Adjusts Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "products#index"
   resources :products
   resources :csv_reports, only: %i[ index show create ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
